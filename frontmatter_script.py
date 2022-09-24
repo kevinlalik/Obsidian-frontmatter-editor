aed204f5302bd25edd1cf0c9a81b3f5a00a8e4b7
Kevin Lalik <kevinlalik@gmail.com>
2022-09-23 22:23:08 -0500

    Added functionality to decline date added frontmatter.

--- frontmatter_script.py
+++ frontmatter_script.py
@@ -4,71 +4,83 @@ from io import BytesIO
 import datetime
 
 notes = []
+keys = ["date_created", "aliases", "tags"]
+vaultPath = "/Users/kevin/Documents/Obsidian notes/Notes copy/"
+newMetadata = {}
 
-# get all .md file paths
+
+# yields each full filename of every .md file in the vaultPath
 def absoluteFilePaths(directory):
-	for dirpath,_,filenames in os.walk(directory):
-		for f in filenames:
-			if f.endswith('md'):
-				yield os.path.abspath(os.path.join(dirpath, f))
-
-allPaths = absoluteFilePaths("/Users/kevin/Documents/Obsidian notes/Notes/")
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            if f.endswith("md"):
+                yield os.path.abspath(os.path.join(dirpath, f))
 
 
+# this function attempts to import the values of the frontmatter key
+# if it doesn't exist it creates a new key value for a given key
+def importKeyValue(note, key):
+    try:
+        print(key)
+        # store value of note's aliases metadata
+        newMetadata[key] = note[key]
+        # if no values, initialize the value of the key as empty,
+        # instead of insterting an empty string
+        if note[key] is None:
+            newMetadata[key] = []
+    # if the key is non-existent, initialize the key-value pair
+    except KeyError:
+        newMetadata[key] = []
 
 
-
-def getKeyValuePairs(filepaths):
-	for i in filepaths:
-		note = frontmatter.load(i)
-		stat = os.stat(i)
-		c_timestamp = stat.st_birthtime
-		c_time = datetime.datetime.fromtimestamp(c_timestamp)
-		obsidian_time = c_time.strftime("%Y-%m-%d %H:%m")
-		newMetadata = {}
-		newMetadata["date_created"] = obsidian_time
-		try:
-			try:
-				newMetadata["aliases"] = note["aliases"]
-				if note["aliases"] is None:
-					newMetadata["aliases"] = []
-			except KeyError: 
-				newMetadata["aliases"] = note["alias"]
-				if note["aliases"] is None:
-					newMetadata["aliases"] = []
-		except:
-			newMetadata["aliases"] = []
-		try:
-			newMetadata["tags"] = note["tags"]
-			if note["tags"] is None:
-					newMetadata["tags"] = []
-		except: 
-			newMetadata["tags"] = []
+def creationDate(filename):
+    stat = os.stat(filename)  # gets info of file
+    c_timestamp = stat.st_birthtime  # stores timestamp of creation time
+    # coverts timestamp into time
+    c_time = datetime.datetime.fromtimestamp(c_timestamp)
+    return c_time.strftime("%Y-%m-%d %H:%m")  # formats time
 
 
-		newNote = frontmatter.Post(content=note.content, date_created=newMetadata["date_created"], aliases=newMetadata["aliases"], tags=newMetadata["tags"])
+# function reads, edits and outputs new metadata to markdown files
+def formatFrontmatter(filepaths):
+    dateInput = input("Put created date as first frontmatter value? (y/n): ")
+    if dateInput == "y":
+        dateSelection = True
+        date = True
+    else:
+        dateSelection = False
+        date = False
+    for i in filepaths:
+        note = frontmatter.load(i)  # loading note using frontmatter import
+        for k in keys:
+            if date is True:
+                newMetadata["date_created"] = creationDate(i)
+                date = False
+            else:
+                importKeyValue(note, k)
+        # create new post from scratch using new metadata
+        date = dateSelection
+        if date is True:
+            newNote = frontmatter.Post(
+                content=note.content,
+                date_created=newMetadata["date_created"],
+                aliases=newMetadata[keys[1]],
+                tags=newMetadata[keys[2]],
+            )
+        else:
+            newNote = frontmatter.Post(
+                content=note.content,
+                aliases=newMetadata[keys[1]],
+                tags=newMetadata[keys[2]],
+            )
+        # saves current file.
+        f = BytesIO()
+        with open(i, "wb") as f:
+            frontmatter.dump(newNote, f, sort_keys=False)
 
-		f = BytesIO()
-		with open(i, 'wb') as f:
-			frontmatter.dump(newNote, f, sort_keys=False)
-	# note.metadata = newMetadata
-	# print(note.metadata)
-	# print(f)
-	# print(note)
-	# with open(filepath, "wb"):
-	# 	frontmatter.dump(newNote, f)
 
-# load each note to posts
-
-newNotes = getKeyValuePairs(allPaths)
-
-# for i in newNotes:
-# 	print(i)
-# 	print(i.metadata)
-# 	with open
-
-
-
-
-
-
+if __name__ == "__main__":
+    # accumulating all filepaths to an array
+    allPaths = absoluteFilePaths(vaultPath)
+    # imports, edits, cleans and initializes all desired frontmatter keys
+    formatFrontmatter(allPaths)
